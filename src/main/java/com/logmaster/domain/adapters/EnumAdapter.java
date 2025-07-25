1cca9293e3d7b533e6e35e92453242e9e34a7d13
Raphael Mobis Tacla <r.mobis@gmail.com>
2025-07-24 23:46:30 -0300

    add matching custom serialization for enums

--- src/main/java/com/logmaster/domain/adapters/EnumAdapter.java
+++ src/main/java/com/logmaster/domain/adapters/EnumAdapter.java
@@ -9,11 +9,16 @@ import java.lang.reflect.Type;
 
 @Slf4j
 @RequiredArgsConstructor
-public class EnumAdapter<T extends Enum<T>> implements JsonDeserializer<T> {
+public class EnumAdapter<T extends Enum<T>> implements JsonDeserializer<T>, JsonSerializer<T> {
     private final Class<T> clazz;
 
     @Override
     public T deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context) throws JsonParseException {
         return EnumUtils.fromString(clazz, jsonElement.getAsString());
+    }
+
+    @Override
+    public JsonElement serialize(T t, Type type, JsonSerializationContext context) {
+        return context.serialize(t.toString(), String.class);
     }
 }
